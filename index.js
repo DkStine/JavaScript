0f928f6f2bedc82e8a870281e14144f09db166ac
DkStine <dkctcak@gmail.com>
2023-09-16 18:57:46 +0530

    FirstJSFile

---
+++ index.js
@@ -0,0 +1,13 @@
+// First js file
+console.log('Hello World')
+
+let ab = 'Deepak Kumar';
+ab = 18;
+console.log(ab)
+
+let person = {
+    nameOf : 'Deepak Kumar',
+    ageOf : 18
+};
+
+console.log(person);
