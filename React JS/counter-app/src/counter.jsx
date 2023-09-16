77d378af3210014fae8d913e0d367f3af69b9f11
DkStine <dkctcak@gmail.com>
2023-09-17 00:12:52 +0530

    Tags

--- React JS/counter-app/src/counter.jsx
+++ React JS/counter-app/src/counter.jsx
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 0
+        count : 1,
+        tags : []
     };
 
-    render() { 
+    render() {
         return(
             <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button><br />
+                {this.state.tags.length === 0 && "Please enter tags!"}
+                {this.renderTags()}
             </div>
         );
+    };
+
+    renderTags(){
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>
+
+        return <ul>
+            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
+    };
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 text-bg-';
+        classes += (this.state.count) === 0 ? 'warning' : 'primary';
+        return classes;
     };
 
     formatCount() {
