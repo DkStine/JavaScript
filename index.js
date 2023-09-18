37b1cecafc4e797ed460cd0aefab60c201278600
DkStine <dkctcak@gmail.com>
2023-09-18 17:13:57 +0530

    JavaScript Essentials for React

--- index.js
+++ index.js
@@ -23,45 +23,76 @@
 // console.log(myVar);
 
 function nextElement(arr, item) {
-    arr.push(item);
-    return arr.shift();
+  arr.push(item);
+  return arr.shift();
 }
 
-var arr = [1, 2, 3, 4, 5]
+var arr = [1, 2, 3, 4, 5];
 
 // console.log('Before: ' + JSON.stringify(arr));
 // console.log('Deleted: ' + nextElement(arr, 6));
 // console.log('After: ', JSON.stringify(arr));
 
 function isItTrue(statement) {
-    if (statement) {
-        return 'Yes, it is true!'
-    } else {
-        return 'No, it is false.'
-    }
+  if (statement) {
+    return "Yes, it is true!";
+  } else {
+    return "No, it is false.";
+  }
 }
 
 // console.log(isItTrue(true));
 
 function checkEquality(char1, char2) {
-    if (char1 !== char2) {
-        return 'Equal';
-    } else {
-        return 'Not equal';
-    }
+  if (char1 !== char2) {
+    return "Equal";
+  } else {
+    return "Not equal";
+  }
 }
 
 // console.log(checkEquality(3, '3'));
 
 function testVal(val) {
-    switch(val) {
-        case 1:
-            return 'alpha';
-        case 2:
-            return 'beta';
-        case 3:
-            return 'gamma';
-    }
+  switch (val) {
+    case 1:
+      return "alpha";
+    case 2:
+      return "beta";
+    case 3:
+      return "gamma";
+  }
 }
 
-console.log(testVal(2));
+// console.log(testVal(2));
+
+const person = {
+  name: "Deepak",
+  age: 18,
+  gender: "male",
+};
+
+// const {name, age, gender} = person;
+
+const personPrime = {
+  ...person,
+  name: "Jagadish",
+};
+
+const { name, age, gender } = personPrime;
+// console.log(name + ' ' + age + ' ' + gender)
+
+/*
+Three most important functions:
+.map()
+.filter()
+.reduce()
+*/
+
+var arr = ["Dk", "Jaggu", "Ali", "Shiva"];
+
+var arr2 = arr.map((each) => {
+  return each + "1";
+});
+
+console.log(arr2);
